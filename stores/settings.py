e08f142257a3f995e79e62f4e2478da450aac1ff
Judekiing <judekiingokorie@gmail.com>
2020-07-28 14:11:00 +0100

    crazy day

--- stores/settings.py
+++ stores/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATTICFILES_DIRS = [os.path.jpoin(BASE_DIR, 'static')]
+STATTICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
