9daeb521db0218b01e6ee8ae39379fe727586092
Jude Okorie <judekiingokorie@gmail.com>
2020-09-30 17:08:08 +0100

    New updates for Heroku deployment

--- stores/settings.py
+++ stores/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2$u)^(_8ohjdn-k)&i429xuj*nl&*vo%5k2irc#u0&#l#igjh0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
