3ed8874c0d3b5e9317648503f7161356d9deada8
Jude Okorie <judekiingokorie@gmail.com>
2020-10-08 13:43:23 +0100

    first

--- stores/settings.py
+++ stores/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATTICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Media Files 
