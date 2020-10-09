844105740c26f830c2b4921403c9ff9e3a3809c9
Jude Okorie <judekiingokorie@gmail.com>
2020-10-09 12:21:24 +0100

    deployment update

--- stores/settings.py
+++ stores/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '2$u)^(_8ohjdn-k)&i429xuj*nl&*vo%5k2irc#u0&#l#igjh0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False, 
 
-ALLOWED_HOSTS = ['dry-river-71657.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
