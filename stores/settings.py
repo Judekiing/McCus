ecdaa202e6286477541cc64dbcc3de7dd24898e0
Jude Okorie <judekiingokorie@gmail.com>
2020-10-09 21:49:16 +0100

    updated setting for deployment

--- stores/settings.py
+++ stores/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = (
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# django_heroku.settings(locals())
+# django_heroku.settings(locals()
 
 
 # Media Files 
