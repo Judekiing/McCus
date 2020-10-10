6e0c9b9b474bf542411d4e0ef61e00f0058f1619
Jude Okorie <judekiingokorie@gmail.com>
2020-10-10 01:25:17 +0100

    updated setting for deployment

--- stores/settings.py
+++ stores/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 
+django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -126,7 +128,6 @@ STATICFILES_DIRS = (
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# django_heroku.settings(locals()
 
 
 # Media Files 
