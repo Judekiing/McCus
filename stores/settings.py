0f0a673ef69bcb7f3f1e2e986aadfa6dccf8a240
Jude Okorie <judekiingokorie@gmail.com>
2020-10-10 18:23:05 +0100

    first

--- stores/settings.py
+++ stores/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '2$u)^(_8ohjdn-k)&i429xuj*nl&*vo%5k2irc#u0&#l#igjh0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = ["herokuapp.com"]
 
 
 # Application definition
